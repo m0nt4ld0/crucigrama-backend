8b68650c87e9802254cc33d81b84f0e666b23d34
Mariela Montaldo <mmontaldo@live.com>
2025-02-01 23:16:00 -0300

    Add: Inicializacion del proyecto

---
+++ src/test/java/com/mmontaldo/crosswords_puzzle/CrosswordsPuzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmontaldo.crosswords_puzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrosswordsPuzzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
